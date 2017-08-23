a249de04101655896e6d77716e997a5cb916a6fa
root <root@fosckaprd01.cer.auckland.ac.nz>
2017-08-24 11:08:47 +1200

    First commit

---
+++ ckanext/cer/plugin.py
@@ -0,0 +1,20 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class CerPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IPackageController, inherit=True)
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'cer')
+
+    def before_search(self, search_params):
+        fq = search_params.get('fq', '')
+	fq += ' +extras_level:"top"'
+	search_params['fq'] = fq
+	 
+        return search_params
---
+++ ckanext/cer/tests/test_plugin.py
@@ -0,0 +1,5 @@
+"""Tests for plugin.py."""
+import ckanext.cer.plugin as plugin
+
+def test_plugin():
+    pass
